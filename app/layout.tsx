412c43d881bb135c0da6af6c1ac5a7b6fa78bd7f
ahmedsatti101 <ahmedysatti@gmail.com>
2025-05-20 14:33:08 +0100

    In progress

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useRef } from "react";
 import "./globals.css";
 
 export default function RootLayout({
@@ -5,13 +8,35 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const bodyRef = useRef<HTMLBodyElement>(null);
+
+  useEffect(() => {
+    const updateMousePosition = (ev: MouseEvent) => {
+      if (!bodyRef.current) return;
+
+      const mouseX = ev.clientX / window.innerWidth;
+      const mouseY = ev.clientY / window.innerHeight;
+      bodyRef.current.style.setProperty("--x", `${mouseX}px`);
+      bodyRef.current.style.setProperty("--y", `${mouseY}px`);
+    };
+
+    window.addEventListener("mousemove", updateMousePosition);
+
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, [])
+  
   return (
     <html lang="en">
       <body
-        className="bg-gradient-to-tl from-[#5d44ad] to-[#2e54ae] bg-no-repeat min-h-screen"
+        ref={bodyRef}
+        //className="bg-[#2c447b] bg-no-repeat min-h-screen"
       >
         {children}
       </body>
     </html>
   );
 }
+
+//background transparent radial-gradient(at calc(var(--mouse-x, 0) * 100%) calc(var(--mouse-y, 0) * 100%), #376ab2, #2c447b) no-repeat 0 0
