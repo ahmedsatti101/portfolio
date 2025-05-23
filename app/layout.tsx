db0d22b25462da858e88e90ac75b81d6716054c3
ahmedsatti101 <ahmedysatti@gmail.com>
2025-05-23 12:34:58 +0100

    Created and positioned header for mobile screens

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useRef } from "react";
 import { Zain } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const zain = Zain({
   subsets: ["latin"],
@@ -40,6 +41,14 @@ export default function RootLayout({
         className={`min-xl:bg-transparent min-xl:rounded-full min-xl:bg-no-repeat min-xl:min-h-screen ${zain.variable} font-sans
                    min-xl:bg-[radial-gradient(at_var(--x)_var(--y),#376ab2_0%,#2c447b_100%)] bg-[#2c447b]`}
       >
+        <title>Ahmed Mohamed</title>
+        <header className="m-2 flex w-fit items-center justify-between">
+          <div className="flex gap-2">
+            <Image src="/brand-github.svg" alt="Github logo" width={24} height={24} />
+            <Image src="/brand-linkedin.svg" alt="Linkedin logo" width={24} height={24}/>
+          </div>
+          <button className="border-1 border-solid rounded-lg px-1">CONTACT ME</button>
+        </header>
         {children}
       </body>
     </html>
