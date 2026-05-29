0eeb449c072c1f3e07809ae525264d3b1ecbaad6
Ahmed Mohamed <ahmedysatti@gmail.com>
2026-05-29 11:04:03 +0100

    first commit

---
+++ data/projects.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    name: "Hunter: Job Tracker",
+    intro: ``,
+    about: ``,
+    stack: ["TypeScript", "React Native", "Expo", "Tailwind", "AWS"],
+    links: ["https://github.com/ahmedsatti101/hunter"],
+    status: "Active development",
+    duration: "Jun 2025 - Present",
+    why: ``
+  }
+]
---
+++ styles/font.ts
@@ -0,0 +1,23 @@
+import { JetBrains_Mono } from "next/font/google";
+
+const jetBrains400 = JetBrains_Mono({
+  weight: '400'
+});
+
+const jetBrains500 = JetBrains_Mono({
+  weight: '500'
+});
+
+const jetBrains600 = JetBrains_Mono({
+  weight: '600'
+});
+
+const jetBrains700 = JetBrains_Mono({
+  weight: '700'
+});
+
+const jetBrains800 = JetBrains_Mono({
+  weight: '800'
+});
+
+export { jetBrains400, jetBrains500, jetBrains600, jetBrains700, jetBrains800 };
