7e4014433742e8833e19549d4ee91d98a3fad8a0
Ahmed Mohamed <ahmedysatti@gmail.com>
2026-05-30 12:38:55 +0100

    Created a footer that contains copyright text and links to my linkedin and github.

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import { jetBrains400, jetBrains800 } from "@/fonts";
 import projects from "../data/projects.js";
 
 export default function Home() {
-  return <p className={`${jetBrains400.className}`}>{projects[0].name}</p>
+  return <p className={`${jetBrains400.className}`}></p>
 }
